735b31263959f2f5f3b4f539f5f0a38cdd269e76
Javier Juarez <javier.juarez@gmail.com>
2014-03-21 08:26:19 +0100

    Configuracion versatil:
     * Soporte para ficheros de configurcion JSON
     * Soporte para configuracion via Hash directa
     * Mejor control de errores
     * Recuperacion de propiedades elegante

--- lib/ldap_codvpnssl/config.rb
+++ lib/ldap_codvpnssl/config.rb
@@ -1,20 +1,64 @@
 require 'yaml'
+require 'json'
 
 
 module LdapCodVPNSSL
 
+
   class Config
 
-    def initialize(config_file)
+    def initialize(source=nil, options={ }, &block)
 
-      @config = YAML.load_file(config_file)
+      @config = Hash.new
+
+      case source
+        when /\.(yml|yaml)/i then @config = YAML.load_file(source)        
+        when /\.json/i       then @config = JSON.parse(File.read(source))
+        when Hash            then @config = source
+
+      else 
+        yield self if block_given?
+      end
 
       self
     end
 
+
     def [](key)
 
       @config[key]
+    end
+
+
+    def []=(key, value)
+
+      @config[key] = value
+    end
+
+
+    def method_missing(method, *arguments, &block)
+
+      attribute = method.to_s
+
+      case attribute
+
+        when /(.+)=$/ then
+          key          = attribute.delete('=').to_sym 
+          @config[key] = (arguments.size == 1 ? arguments[0] : arguments)
+
+        when /(.+)\?$/ then
+          key = attribute.delete('?').to_sym
+          @config.keys.include?(key) 
+
+        else
+          if @config.keys.include?(method)
+            
+            @config[method]
+          else
+            
+            super
+          end
+      end
     end
   end
 end
