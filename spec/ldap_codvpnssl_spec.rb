43cdee9da8e21a4cac95e88b9be2f0250fce8fc8
Javier Juarez <javier.juarez@gmail.com>
2014-03-21 01:16:39 +0100

    Corregido el nombre del modulo

---
+++ spec/ldap_codvpnssl_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe LdapCodVPNSSL do
+
+  it 'should have a version number' do
+    LdapCodVPNSSL::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should eq(true)
+  end
+end
