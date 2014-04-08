224a773838b371b7abb7ff8c2cf67c0d9c7c752c
Javier Juarez <javier.juarez@gmail.com>
2014-04-09 00:41:55 +0200

    CAmbios:
     * Control de errores independiente del comando
     * Metodo pretty_print para entradas LDAP
     * Opciones a nivel de comando

--- lib/ldapam/cli.rb
+++ lib/ldapam/cli.rb
@@ -1,41 +1,74 @@
 # encoding: utf-8
 require "thor"
 require "ldapam/config"
-require "ldapam/client"
+require "ldapam/connection"
 
 
 module LDAPAM
 
   class CLI < Thor
 
-    class_option :config, :aliases =>"-c", :type =>:string
+    def self.die(exception, code=0)
+    
+      STDERR.puts "ERROR: #{exception.class}: '#{exception.message}'"
+      ::Kernel.exit(code)
+    end
+
+
+    def self.pp_entry(entry, attributes)
+
+      attributes.each do |attribute|
+
+        if entry[attribute].length > 1
+
+          puts "#{attribute}: #{entry[attribute].join(",")}"
+        else
+
+          puts "#{attribute}: #{entry[attribute][0]}"
+        end
+      end
+    end
+
 
     desc "get uid attribute", "Show the attribute for the uid"
+    method_option :config, :type =>:string, :required =>true
     def get(uid, attribute)
 
-      ldap      = Client.new(Config.new(options[:config]))
-      resultset = ldap.find_by_uid(uid, [attribute])
+      config    = Config.new(options[:config])
+      ldap      = Connection.new(config)
+      resultset = ldap.find_by(:uid, uid, [attribute])
 
-      resultset.each { |e| puts "#{attribute}: #{e[attribute]}" }
+      resultset.each { |entry| CLI.pp_entry(entry, attribute) }
+    rescue Exception =>exc
+      CLI.die(exc, 1) 
     end
 
 
     desc "set uid attribute value", "Replace the attribute for the uid"
+    method_option :config, :type =>:string, :required =>true
     def set(uid, attribute, value)
 
-      ldap      = Client.new(Config.new(options[:config]))
-      resultset = ldap.find_by_uid(uid, [:dn])
+      config    = Config.new(options[:config])
+      ldap      = Connection.new(config)
+      resultset = ldap.find_by(:uid, uid, [:dn])
 
-      resultset.each { |e| ldap.update(e.dn, attribute, value) }
+      resultset.each { |entry| ldap.update(entry.dn, attribute, value) }
+    rescue Exception =>exc 
+      CLI.die(exc, 2) 
     end
 
+
     desc "showuids attribute value", "Replace the attribute for the uid"
-    def showuids(attribute, value)
+    method_option :config, :type =>:string, :required =>true
+    def match(attribute, value, *attributes)
 
-      ldap      = Client.new(Config.new(options[:config]))
-      resultset = ldap.find_by(:codVPNSSL, value, [:uid])
+      config    = Config.new(options[:config])
+      ldap      = Connection.new(config)
+      resultset = ldap.find_by(attribute, value, attributes)
 
-      resultset.each { |e| puts "#{e['uid'][0]}" }
+      resultset.each { |entry| puts entry.inspect; CLI.pp_entry(entry, attributes) }
+    rescue Exception =>exc
+      CLI.die(exc, 3) 
     end
   end
 end
